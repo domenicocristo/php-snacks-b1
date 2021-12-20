dd37738aef2ecf2646465e37961ec17bf68617f8
MyUsername <my@email.com>
2021-12-20 15:56:52 +0100

    snack-1

---
+++ snack-1/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-snacks-b1</title>
+    <?php
+    $partita1 = [
+        $squadraCasa = [
+            "punti" => "20"
+        ],
+        $squadraOspite = [
+            "punti" => "10"
+        ]
+    ];
+    $partita2 = [
+        $squadraCasa = [
+            "punti" => "30"
+        ],
+        $squadraOspite = [
+            "punti" => "20"
+        ]  
+    ];
+    ?>
+</head>
+<body>
+    <div>
+        <h1>Partita 1</h1>
+
+        <h3>Squadra casa</h3>
+        <div>
+            <?php
+            echo "punti"
+            ?>
+        </div>
+
+        <h3>Squadra ospite</h3>
+        <div>
+            <?php
+            echo
+            ?>
+        </div>
+    </div>
+</body>
+</html>
